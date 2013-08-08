b79973a2781d158d2318a83b24b71908d70ea294
maparrar <maparrar@gmail.com>
2013-08-07 21:52:48 -0500

    Nueva vista para pruebas

---
+++ engine/views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <?php
+            //Incluye los scripts
+            Router::js("jquery");
+        ?>
+        <title>Landing | Maqinato</title>
+        <script  type="text/javascript">
+            $(document).ready(function(){
+                $("body").click(function(){
+                  $(this).css("background","red");
+                });
+              });
+        </script>
+    </head>
+    <body>
+        
+    </body>
+</html>
