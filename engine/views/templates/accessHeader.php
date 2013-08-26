545468760d40d9c4bfadcf6c1fd57cfafe45b8e4
maparrar <maparrar@gmail.com>
2013-08-26 05:37:37 -0500

    .

--- engine/views/templates/accessHeader.php
+++ engine/views/templates/accessHeader.php
@@ -12,8 +12,8 @@
 <header>
     <h1 class="logo"><a href="<?php echo Router::path('root'); ?>">maqinato</a></h1>
     <div id="loginForm">
-        <div class="field"><div class="name"><?php echo _("Email"); ?>:</div><input type="text" name="email"/></div>
-        <div class="field"><div class="name"><?php echo _("Password"); ?>:</div><input type="password" name="password"/></div>
+        <div class="field"><input type="text" name="email" placeholder="<?php echo _("Email"); ?>"/></div>
+        <div class="field"><input type="password" name="password" placeholder="<?php echo _("Password"); ?>"/></div>
         <div id="login" class="field button"><?php echo _("Login"); ?></div>
     </div>
 </header>
